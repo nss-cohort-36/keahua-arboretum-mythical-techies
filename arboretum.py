07fbead755f828c83ee63c10b133825ddfa32e0d
Jesie Oldenburg <oldesdev@outlook.com>
2020-01-27 15:52:03 -0600

    Began work on generating arboretum summary report.

--- arboretum.py
+++ arboretum.py
@@ -13,47 +13,6 @@ class Arboretum:
         self.avail_habitats = avail_habitats
         self.habitats_dict = {habitat: [] for habitat in avail_habitats}
 
-    @property
-    def rivers(self):
-        return self.__rivers
-
-    def annex_river(self, river):
-        self.__rivers.append(river)
-
-    @property
-    def grasslands(self):
-        return self.__grasslands
-
-    def annex_grassland(self, grassland):
-        self.__grasslands.append(grassland)
-
-    @property
-    def mountains(self):
-        return self.__mountains
-
-    def annex_mountain(self, mountain):
-        self.__mountains.append(mountain)
-
-    @property
-    def swamps(self):
-        return self.__swamps
-
-    def annex_swamp(self, swamp):
-        self.__swamps.append(swamp)
-
-    @property
-    def forests(self):
-        return self.__forests
-
-    def annex_forest(self, forest):
-        self.__forests.append(forest)
-
-    @property
-    def coastlines(self):
-        return self.__coastlines
-
-    def annex_coastline(self, coastline):
-        self.__coastlines.append(coastline)
 
     def annex_habitat(self):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -64,7 +23,7 @@ class Arboretum:
         choice = input("Choose your habitat > ")
         print(choice)
 
-    # Add other biomes
+        # Add other biomes
 
         if choice == "1":
             habitat = Mountain()
